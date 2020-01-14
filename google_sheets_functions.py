12baaeca6f7f7fe35555e022aea7fce23130f0e3
Tornike <tornike.skhulukhia.1@iliauni.edu.ge>
2020-01-14 12:38:59 +0400

    Create google_sheets_functions.py

---
+++ google_sheets_functions.py
@@ -0,0 +1,24 @@
+'''
+    functions to work with google sheets using gspread library
+'''
+
+def login_to_google_sheets(credentials_file):
+    '''
+    returns authenticated client object that we can use
+    with gspread library.
+
+    arguments:
+        1. credentials -json file of login credentials
+    '''
+    import gspread
+    from oauth2client.service_account import ServiceAccountCredentials as SAC
+
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    credentials = SAC.from_json_keyfile_name(
+                        credentials_file, scope)
+
+    client = gspread.authorize(credentials)
+
+    return client
